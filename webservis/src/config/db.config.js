6184bec599ec79c88c58dda22ae29961bdc27c95
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-07-24 16:04:50 +0300

    dd
    dd

---
+++ webservis/src/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = { 
+  database: "Juniorsuperligdb",
+  //url: "mongodb://37.77.4.139:27017/dogsandb",
+  imgBucket: "photos",
+  videoBucket: "fs",
+   
+};
---
+++ webservis/src/routes/user.routes.js
@@ -0,0 +1,37 @@
+const { authJwt } = require("../middlewares");
+const controller = require("../controllers/user.controller");
+
+var multer = require('multer');
+
+/* var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+    cb(null, file.fieldname + '-' + Date.now() + ext)
+  }
+}); */
+
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get("/api/test/all", controller.allAccess);
+
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+ 
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
+};
